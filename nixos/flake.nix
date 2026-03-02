27cc855b35ebcf63defa1bde7ecd9493769b13ad
AngelicAstronaut <204285800+AngelicAstronaut@users.noreply.github.com>
2026-03-02 23:41:18 +0000

    Update flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,11 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {self, nixpkgs, ...} @ inputs: {
     nixosConfigurations.astronaut = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      specialArgs = { inherit inputs; }; # this is the important part
       modules = [
         ./configuration.nix
         ./hardware-configuration.nix
