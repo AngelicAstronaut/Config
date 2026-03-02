a25c5fbefe84664aa0e21a2d44e68e53597836ee
AngelicAstronaut <204285800+AngelicAstronaut@users.noreply.github.com>
2026-03-02 09:55:52 -0500

    flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,1 +1,17 @@
+{
+  description = "I use Nixos btw";
 
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.astronaut = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        ./hardware-configuration.nix
+      ];
+    };
+  };
+}
